ec9658dd945488f49a7b79690ca35e751abc096d
mennaabduh <151149305+mennaabduh@users.noreply.github.com>
2023-11-17 14:40:53 +0200

    project upload

---
+++ story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qvQzSZkwJT":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById("slide").style.cursor = "url('brush.jpg'),auto"
+}
+
